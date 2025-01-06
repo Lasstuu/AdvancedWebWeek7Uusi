c223f3718c2cc5d9ccdaee3ea567d43470aa1b01
lasstuu <lassituo@gmail.com>
2025-01-06 18:13:51 +0200

    asd

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const users: IUser[] = [];
 
 
 router.post("/api/user/register",
-    body("email").isEmail(),
+    body("email").isEmail().escape(),
     body("password"),
     async (req: Request, res: Response):Promise<any> => {
         const errors: Result<ValidationError> = validationResult(req)
@@ -47,7 +47,36 @@ router.post("/api/user/register",
 
 })
 
-router.get('/api/user/list', async (req: Request, res: Response):Promise<any> => {
+router.get("/api/user/list", async (req: Request, res: Response):Promise<any> => {
     return res.status(200).json(users);
 });
+
+router.post("/api/user/login"), 
+body("email").trim().escape(),
+body("password").escape(),
+async (req: Request, res: Response) =>{
+    try {
+        const user: IUser | undefined = users.find(user => user.email === req.body.email)
+
+        if (!user){
+            return res.status(401).json({message: "Login failed"})
+        }
+        if(bcrypt.compareSync(req.body.password, user.password)){
+            const JwtPayload: JwtPayload ={
+                email: user.email
+            }
+            const token: string = jwt.sign(JwtPayload, process.env.SECRET as string, { expiresIn: "2m"})
+
+            return res.status(200).json({success: true, token})
+        }
+        return res.status(401).json({message: "Login failed"})
+
+    } catch(error: any){
+        console.error(`Error during user login: ${error}`)
+        return res.status(500).json({ error: "Internal server error" })
+    }
+
+
+
+}
 export default router
